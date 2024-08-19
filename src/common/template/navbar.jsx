37c68af2d2bdc361ee992f15756f7950095a4683
DavidIsrael <davidisrael.rj@gmail.com>
2024-08-19 18:51:30 -0300

    recolocando a imagem

--- src/common/template/navbar.jsx
+++ src/common/template/navbar.jsx
@@ -14,6 +14,7 @@ class Navbar extends Component {
     }
 
     render() {
+        const loginImg = imgLogin
         const { name, email } = this.props.user
         return (
             <div className="navbar-custom-menu">
@@ -25,13 +26,13 @@ class Navbar extends Component {
                             aria-expanded={this.state.open ? 'true' : 'false'}
                             className="dropdown-toggle"
                             data-toggle="dropdown">
-                            <img src="http://lorempixel.com/160/160/abstract"
+                            <img src={loginImg}
                                 className="user-image" alt="User Image" />
                             <span className="hidden-xs">{name}</span>
                         </a>
                         <ul className="dropdown-menu">
                             <li className="user-header">
-                                <img src="http://lorempixel.com/160/160/abstract"
+                                <img src={loginImg}
                                     className="img-circle" alt="User Image" />
                                 <p>{name}<small>{email}</small></p>
                             </li>
